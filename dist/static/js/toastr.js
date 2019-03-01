bc71077d5f28d9306b0132cc9b296f5aec545262
Manikanta1992 <manikanta@strategy64.com>
2019-03-01 23:46:35 +0530

    Second Commit

---
+++ dist/static/js/toastr.js
@@ -0,0 +1,71 @@
+/*
+Template Name: Monster Admin
+Author: Themedesigner
+Email: niravjoshi87@gmail.com
+File: js
+*/
+$(function() {
+    "use strict";
+      $(".tst1").click(function(){
+           $.toast({
+            heading: '9491330953',
+            text: '9B7V1Q',
+            position: 'top-right',
+            loaderBg:'#ff6849',
+            icon: 'info',
+            hideAfter: 3500,
+            stack: 6
+          });
+
+     });
+
+      $(".tst2").click(function(){
+           $.toast({
+            heading: '9440503796',
+            text: '675UI8',
+            position: 'top-right',
+            loaderBg:'#ff6849',
+            icon: 'warning',
+            hideAfter: 3500,
+            stack: 6
+          });
+
+     });
+      $(".tst3").click(function(){
+           $.toast({
+            heading: '8374567724',
+            text: '825QC4',
+            position: 'top-right',
+            loaderBg:'#ff6849',
+            icon: 'success',
+            hideAfter: 3500,
+            stack: 6
+          });
+
+     });
+
+      $(".tst4").click(function(){
+           $.toast({
+            heading: '9550764561',
+            text: '715FA2',
+            position: 'top-right',
+            loaderBg:'#ff6849',
+            icon: 'error',
+            hideAfter: 3500,
+            stack: 6
+          });
+
+  });
+          $(".tst5").click(function(){
+               $.toast({
+                heading: '9000050102',
+                text: '715FA2',
+                position: 'top-right',
+                loaderBg:'#ff6849',
+                icon: 'error',
+                hideAfter: 3500,
+                stack: 6
+
+     });
+});
+});
